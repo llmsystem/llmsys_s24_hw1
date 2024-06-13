9ae9ac2f952674e80511ca95d4fd8ca51112a27e
klhhhhh <1412841649@qq.com>
2024-06-13 16:29:39 +0800

    test author

--- src/combine.cu
+++ src/combine.cu
@@ -244,6 +244,8 @@ __global__ void MatrixMultiplyKernel(
     // 6. Synchronize to make sure all threads are done computing the output tile for (row, col)
     // 7. Write the output to global memory
 
+    
+
     assert(false && "Not Implemented");
     /// END ASSIGN1_2
 }
